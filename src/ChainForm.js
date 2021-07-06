c62e7008e3e3f36fa473ed31ec45635fb4210507
Hadrien Croubois <hadrien.croubois@gmail.com>
2021-07-06 12:29:29 +0200

    add more endpoints

--- src/ChainForm.js
+++ src/ChainForm.js
@@ -7,22 +7,27 @@ import {
   FormControl,
 } from 'react-bootstrap'
 
-const CHAINS = [
-  'mainnet',
-  'ropsten',
-  'rinkeby',
-  'goerli',
-  'kovan',
-]
+const CHAINS = {
+  'mainnet':          'mainnet',
+  'ropsten':          'ropsten',
+  'rinkeby':          'rinkeby',
+  'goerli':           'goerli',
+  'kovan':            'kovan',
+  'bsc':              'https://bsc-dataseed.binance.org/',
+  'bsc-testnet':      'https://data-seed-prebsc-1-s1.binance.org:8545/',
+  'xdai':             'https://dai.poa.network',
+  'polygon':          'https://rpc-mainnet.maticvigil.com/',
+  'arbitrum-testnet': 'https://rinkeby.arbitrum.io/rpc',
+}
 
 const DEFAULT_ENPOINT = 'http://localhost:8545'
 
 const ChainForm = (props) => {
-  const [ chain,    setProvider ] = React.useState(CHAINS.find(Boolean))
+  const [ chain,    setProvider ] = React.useState(Object.keys(CHAINS).find(Boolean))
   const [ endpoint, setEndpoint ] = React.useState(DEFAULT_ENPOINT)
 
   const connect = () => {
-    const path = encodeURIComponent(chain === "custom" ? endpoint : chain)
+    const path = encodeURIComponent(chain in CHAINS ? CHAINS[chain] : endpoint)
     props.routing.history.push(`/${path}`)
   }
 
@@ -30,7 +35,7 @@ const ChainForm = (props) => {
     <>
       <InputGroup className='justify-content-md-center mb-3'>
         <FormControl as='select' onChange={event => setProvider(event.target.value)}>
-          { CHAINS.map((chain, i) => <option key={i}>{chain}</option>) }
+          { Object.keys(CHAINS).map((chain, i) => <option key={i}>{chain}</option>) }
           <option key={CHAINS.length}>custom</option>) }
         </FormControl>
       </InputGroup>
